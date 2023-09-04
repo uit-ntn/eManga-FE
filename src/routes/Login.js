b48cd85a35047bb986eac0fa832694e8259decf9
Nguyễn Thanh Nhân <npthanhnhan2003@gmail.com>
2023-09-05 02:12:33 +0700

    fix homepage bug

--- src/routes/Login.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-function Login (){
-    return (
-        <div className="Login">Login </div>
-    )
-}
-export default Login;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,15 +2,13 @@ import Home from './Home';
 import Contact from './Contact';
 import Admin from './Admin';
 import About from './About';
-import Login from './Login';
 import Detail from './Detail';
 
 
 const publicRoutes = [
 {path :'/'  ,component : Home},
 {path :'/contact',component:Contact},
-{path :'/about',component:About},
-{path:'/login', component:Login},
+{path :'/about',component:About},,
 {path:'/detail', component:Detail}
 ];
 
