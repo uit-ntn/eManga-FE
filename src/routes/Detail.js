aa72d9beb119dee70421284cca28c05646cfee60
dtee <93820286+DT2K3@users.noreply.github.com>
2023-09-04 12:02:40 +0700

    update detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -7,7 +7,7 @@ function Detail() {
     return (
         <Layout>
             <div className="manga-detail">
-                <div className="manga-card">
+                <div className="manga-card background">
                     <div className="manga-card__image">
                         <img src="https://loremflickr.com/cache/resized/65535_52559459065_f72e7e59d1_b_640_480_nofilter.jpg"></img>
                     </div>
@@ -16,10 +16,17 @@ function Detail() {
                             <div className="manga-card__detail-name">Abyss Rage</div>
                             <div className="manga-card__detail-author">Roosevelt Brown</div>
                         </div>
-                        <div className="manga-card__detail-genre">Thể loại: Metal</div>
-                        <div className="manga-card__detail-rating">Rating: 25</div>
+                        <div className="manga-card__detail-genre">
+                            <span className="Manga-card__label">Thể loại:</span>
+                            <span className="Manga-card__value">Metal</span>
+                            </div>
+                        <div className="manga-card__detail-rating">
+                        <span className="Manga-card__label">Rating: 
+                        <span className="Manga-card__value">25</span>
+                        </span>
+                        </div>
                         <div className="manga-card__buttons">
-                            <button className="favorite-btn">Add to favorite</button>
+                            <button className="favorite-btn">+ Add to favorite</button>
                             <button className="rating-btn">Rating</button>
                             <ul className="manga-card__share-buttons">
                                 <li className="share-button">
@@ -40,6 +47,10 @@ function Detail() {
                             </ul>
                         </div>
                     </div>
+                </div>
+                <div className="tab-heading">
+                <span className="tab-link tab-heading__manga-content-tab">Nội Dung Truyện</span>
+                <span className="tab-link tab-heading__comments-tab">Bình Luận</span>
                 </div>
                 <div className="content-container">Repellat corporis qui fugit laborum cum eos repudiandae. Quod corporis inventore a. Voluptatibus autem molestias soluta.\nRerum laudantium molestiae sapiente dolores ducimus neque. Nisi aspernatur doloremque accusantium. Amet consequuntur non molestiae. Occaecati dolore assumenda totam accusantium optio consequatur quas nesciunt.\nFugiat consequatur vero corporis repudiandae asperiores dolorum. Libero nostrum voluptatibus ea culpa nisi. Dolore omnis illo labore commodi alias. Eum hic minima autem repellat temporibus et eum quam. Iusto nisi omnis. Non nemo impedit cum voluptates perspiciatis assumenda porro.</div>
             </div>
